8107604cf7dddf58212299af3f2ec197835fc085
Lukas <lburg5534@gmail.com>
2023-12-20 18:47:19 +0100

    Remove duplicate function

--- src/linux_connector.rs
+++ src/linux_connector.rs
@@ -1,7 +1,5 @@
 pub mod linux_connector {
     use std::process::Command;
-    use std::thread;
-    use std::time::Duration;
     use crate::connector::connector::Connector;
 
     pub struct LinuxVpnConnector<'a>{
@@ -43,33 +41,6 @@ pub mod linux_connector {
                 Err(output) => {
                     println!("Failed to execute command: {}", output);
                     false
-                }
-            }
-        }
-
-        fn run_vpn_connector(&self, connections: &Vec<String>){
-            loop {
-                let mut connected: bool = false;
-                println!("Check connection status...\\n");
-
-                for connection in &connections[2..] {
-                    connected = self.is_connected(connection);
-
-                    if connected {
-                        println!("Status: {}OK\\n{}Connected to secure network\\n", "\\x1B[32m", "\\x1B[0m");
-                        thread::sleep(Duration::from_secs(30));
-                        break;
-                    }
-                }
-
-                if !connected {
-                    println!("Status: {}NOT CONNECTED{}\\nTrying to connect...\\n", "\\x1B[31m", "\\x1B[0m");
-                    let success: bool = self.connect_to_vpn();
-
-                    if !success {
-                        println!("Failed to connect to VPN: Check VPN name");
-                        return;
-                    }
                 }
             }
         }
