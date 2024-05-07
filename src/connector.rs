39d67246798e370b3551a8f13d8891a71041f643
Lukas B <91851884+LukasB24@users.noreply.github.com>
2024-05-07 20:37:40 +0200

    Fix: Connect to vpn (#1)
    VPN is disabled when connected to vpn and home network at the same time

--- src/connector.rs
+++ src/connector.rs
@@ -9,22 +9,46 @@ pub mod connector {
 
         fn connect_to_vpn(&self) -> bool;
 
+        fn disconnect_from_vpn(&self) -> bool;
+
         fn run_vpn_connector(&self, connections: &Vec<String>){
             loop {
-                let mut connected: bool = false;
+                let mut connected_to_secure_network: bool = false;
+                let connected_to_vpn: bool = self.is_connected(&connections[1]);
+                let connected_to_home_network: bool = self.is_connected(&connections[2]);
+
                 println!("Check connection status...\n");
-        
-                for connection in &connections[2..] {
-                    connected = self.is_connected(connection);
-        
-                    if connected {
-                        println!("Status: {}OK\n{}Connected to secure network\n", "\x1B[32m", "\x1B[0m");
-                        thread::sleep(Duration::from_secs(30));
-                        break;
+                
+                if connected_to_home_network {
+                    if connected_to_vpn {
+                        println!("Status: {}OK\n{}Connected to VPN and home network. VPN will be disabled\n", "\x1B[32m", "\x1B[0m");
+                        self.disconnect_from_vpn();
+                    }
+                    else{
+                        println!("Status: {}OK\n{}Connected to home network: {}\n", "\x1B[32m", "\x1B[0m", &connections[2]);
+                    }
+                    thread::sleep(Duration::from_secs(30));
+                    continue;
+                } else if connected_to_vpn {
+                    println!("Status: {}OK\n{}Connected to VPN\n", "\x1B[32m", "\x1B[0m");
+                    thread::sleep(Duration::from_secs(30));
+                    continue;
+                }
+                
+                if connections.len() > 3 { // additional networks were specified and need to be checked
+                    for connection in &connections[3..] {
+                        connected_to_secure_network = self.is_connected(connection);
+                        println!("Checking connection to secure network: {}\n", connection);
+    
+                        if connected_to_secure_network {
+                            println!("Status: {}OK\n{}Connected to secure network: {}\n", "\x1B[32m", "\x1B[0m", connection);
+                            thread::sleep(Duration::from_secs(30));
+                            break;
+                        }
                     }
                 }
-        
-                if !connected {
+                
+                if !connected_to_secure_network {
                     println!("Status: {}NOT CONNECTED{}\nTrying to connect...\n", "\x1B[31m", "\x1B[0m");
                     let success: bool = self.connect_to_vpn();
         
@@ -32,9 +56,11 @@ pub mod connector {
                         println!("Failed to connect to VPN: Check VPN name");
                         return;
                     }
+
+                    println!("Status: {}OK\n{}Successfully connected to VPN\n", "\x1B[32m", "\x1B[0m");
+                    thread::sleep(Duration::from_secs(30));
                 }
             }
         }
     }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::thread;
 use std::time::Duration;
 use linux_connector::linux_connector::LinuxVpnConnector;
 use connector::connector::Connector;
+
 mod linux_connector;
 mod connector;
 
@@ -12,7 +13,8 @@ fn main() {
 
     if args.len() < 3 {
         println!("First argument is VPN name.\nYou need to provide at least one WIFI-SSID as argument.\
-        \n\nUsage: ./vpn_connector <VPN-name> <connection name>");
+        \nBeware that first WIFI-SSID is interpreted as home network, that your VPN points to. 
+        \nUsage: ./vpn_connector <VPN-name> <home network name> [optional] <secure network 1> <secure network 2>");
         return;
     }
 
